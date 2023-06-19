11fff0879720d5ecb480f4054c958430956db871
Matheus de Arruda Ferreira <matheusarrudaf@gmail.com>
2023-06-19 15:50:51 -0300

    second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
-import Image from 'next/image'
+import Head from 'next/head'
+
+
 
 export default function Home() {
+
+  <Head>
+    <title>Daily News</title>
+  </Head>
+
   return (
     <h1>Hello There!</h1>
   )
