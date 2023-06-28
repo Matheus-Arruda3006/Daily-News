041586a52c2b52c29ecbcc1110ef16a963ccba05
Matheus-Arruda3006 <matheusarrudaf@gmail.com>
2023-06-27 22:22:00 -0300

    faunadb commit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,14 @@
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
 import styles from './styles.module.scss';
-import {signIn, useSession} from 'next-auth';
+import {signIn, signOut,useSession} from 'next-auth/react';
 
 export function SignInButton(){
 
-    const [session] = useSession();
+    const {data: session} = useSession()
 
     return session? (
-        <button type="button" className={styles.signInButton}> <FaGithub color='#04d361'/> Matheus de Arruda <FiX color='#737380' className={styles.closeIcon}/></button>
+        <button type="button" className={styles.signInButton} onClick={() => signOut()}> <FaGithub color='#04d361'/> {session.user?.name} <FiX color='#737380' className={styles.closeIcon}/></button>
     ) : (
         <button type="button" className={styles.signInButton} onClick={() => signIn('github')}> <FaGithub color='#eba417'/> Sign in with GitHub</button>
     )
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,12 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
-export const authOptions = {
+
+import { fauna } from '../../../services/fauna';
+
+
+export default NextAuth ({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
@@ -8,7 +14,19 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-    // ...add more providers here
   ],
-}
-export default NextAuth(authOptions)
+  callbacks: {
+    async signIn(user, account, profile){
+
+        const {email} = user
+
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: { email }}
+          )
+        )
+    }
+  },
+})
+
